6a71b71716e8390a2ad036d11307f8b189459125
paoladisouza4 <paoladisouza4@gmail.com>
2026-04-15 04:05:39 -0300

    Delete src/components/layout/ui directory

--- src/components/layout/ui/Notif.jsx
+++
@@ -1,8 +0,0 @@
-export default function Notif({ notif }) {
-  return (
-    <div className={`notif${notif ? ' show' : ''}`}>
-      <span>{notif?.icon}</span>
-      <span>{notif?.msg}</span>
-    </div>
-  )
-}
